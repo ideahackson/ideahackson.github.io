15007de3153e2d5483997613b461af1436f693f6
Fuya Yamada <fuyamiwa713@gmail.com>
2024-07-30 09:44:14 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: ['./*.html'],
+  theme: {
+    extend: {},
+    minWidth: {
+      '40': '10rem',
+    },
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+      '8xl': '6rem',
+    }
+  },
+  variants: {},
+  plugins: []
+}
